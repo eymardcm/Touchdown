5b663ad236bb41da1740ec422c6970252ed44a0b
Chad Eymard <eymardcm@gmail.com>
2024-03-10 20:54:50 -0400

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Chad Eymard on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
